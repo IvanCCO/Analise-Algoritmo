20065f2c392cd0fd9b47c1a6a20ba36b23db5097
IvanCCO <ivanmedeiros0903@outlook.com>
2024-02-24 13:12:40 +0000

    grafico velocimetro

--- Velocimetro/main.py
+++ Velocimetro/main.py
@@ -4,6 +4,8 @@ from datetime import datetime
 from memory_profiler import memory_usage
 import matplotlib.pyplot as plt
 import numpy as np
+import random
+import math
 
 
 def sum_of_n_init(n):
@@ -27,14 +29,28 @@ def multilply_by_hour() -> int:
     return now
 
 
+def multilply_by_seconds() -> int:
+    now = datetime.now().time().second
+    if now <= 0:
+        return 1
+    return now
+
+
+def multilply_by_microseconds() -> int:
+    now = datetime.now().time().microsecond
+    if now <= 0:
+        return 1
+    return now
+
+
 def separator():
     print("=" * 100)
 
 
-def plot_grafico_de_linha_2(y1, y2):
+def plot_grafico_de_linha_2(y1, x1):
     while True:
         plt.plot(
-            list(range(len(y1))), y1, label="sum_of_n_init"
+            x1, y1, label="sum_of_n_init"
         )  # Linha para o primeiro conjunto de dados
         plt.xlabel("Tamanho do input (n)")
         plt.ylabel("Tempo gasto")
@@ -47,24 +63,32 @@ def plot_grafico_de_linha_2(y1, y2):
 
 if __name__ == "__main__":
     tempo_y = []
-    memoria_y = []
-    multiplicator = 1
+    x = []
     counter = 0
+
+    m = 1
+    n = 1
+
     try:
         while True:
-            if counter > 500:
-                counter = 0
-                multiplicator += multiplicator
-            cicle = multilply_by_minute() * multilply_by_hour() * multiplicator
+            num = max(m, n)
+            m = random.randint(num, (num + random.randint(1, num)))
+            n = random.randint(num, (num + random.randint(1, num)))
+            if multilply_by_minute() > 30:
+                acelerador = m * n * random.randint(1, 100)
+            else:
+                acelerador = m * n
+
+            print(f"Valores de multiplicação serão {m} * {n}")
             inicio = time.time()
-            sum_of_n_init(cicle)
+            sum_of_n_init(acelerador)
             fim = time.time()
             tempo = round(fim - inicio, 2)
             tempo_y.append(tempo)
-            # memoria_y.append(memoria)
-            print(f"Tempo gasto para percorrer {cicle} iterações = {tempo}")
+            x.append(acelerador)
+            print(f"Acelerador = {acelerador}, tempo = {tempo}")
             counter += 1
     except Exception as e:
         print(e)
     finally:
-        plot_grafico_de_linha_2(tempo_y, memoria_y)
+        plot_grafico_de_linha_2(tempo_y, x)
