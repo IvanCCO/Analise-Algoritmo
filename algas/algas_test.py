39d292616213dfaf9c6b4184d8785438e47d0235
IvanCCO <ivan.miranda@sptech.school>
2024-04-19 18:10:40 -0300

    add memory consumption

--- algas/algas_test.py
+++ algas/algas_test.py
@@ -1,5 +1,6 @@
 import time
 import csv
+import psutil  # Importe psutil
 
 
 def iterate(arr):
@@ -19,9 +20,9 @@ def execute_first_block(step):
 def main():
     initial_step = 10**6
     step = initial_step
-    max_time = 10  # Tempo máximo desejado
     tempo = []
     step_arr = []
+    memory_usage = []
 
     while True:
         inicio = time.time()
@@ -35,21 +36,25 @@ def main():
         tempo.append(time_result)
         step_arr.append(step)
 
+        memory_usage.append(psutil.Process().memory_info().rss)
+
         step -= int(step * 0.1)
 
-        if step < 1:
+        if step < 100:
             print("Step chegou a um valor muito baixo.")
             break
 
-    save_to_csv(step_arr, tempo)
+    save_to_csv(step_arr, tempo, memory_usage)
 
 
-def save_to_csv(x, y):
+def save_to_csv(x, y, memory):
     with open("data.csv", mode="w", newline="") as file:
         writer = csv.writer(file)
-        writer.writerow(["Tamanho do step (n)", "Tempo gasto"])
+        writer.writerow(
+            ["Tamanho do step (n)", "Tempo gasto", "Uso de Memória (bytes)"]
+        )
         for i in range(len(x)):
-            writer.writerow([x[i], y[i]])
+            writer.writerow([x[i], y[i], memory[i]])
 
 
 if __name__ == "__main__":
